d4a32d959371f1a6e2ab397289321d2a9a80c2ad
Crizomb <clementbart.45@gmail.com>
2024-12-22 21:42:27 +0100

    Add camera movement

---
+++ Assets/Scripts/CameraMouvement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMouvement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private Vector2 _moveInput;
+    public void HandleCameraMovement(InputAction.CallbackContext context)
+    {
+        _moveInput = Vector2.zero;
+        if (context.phase == InputActionPhase.Performed)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouvement = moveSpeed * Time.deltaTime * (new Vector3(_moveInput.y, 0, -_moveInput.x));
+        transform.Translate(mouvement, Space.World);
+    }
+}
