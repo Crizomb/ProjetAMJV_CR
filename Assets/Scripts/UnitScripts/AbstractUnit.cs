7287be07a9b529990cc307df3c4b6f70f11582ee
Crizomb <clementbart.45@gmail.com>
2025-01-13 17:40:00 +0100

    Delete attestation de niveau allemand LV2
    wtf but real

--- Assets/Scripts/UnitScripts/AbstractUnit.cs
+++ Assets/Scripts/UnitScripts/AbstractUnit.cs
@@ -4,6 +4,7 @@ public abstract class AbstractUnit : MonoBehaviour
 {
     public float price;
     [field: SerializeField] public bool IsTeamA { get; private set; }
+    [field: SerializeField] public bool IsQueen { get; private set; }
     public abstract bool Attack();
     public abstract void TakeDamage(float damage);
     
