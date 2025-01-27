afe60bae57e965a3217d6fec70079542f35c79b8
Crizomb <clementbart.45@gmail.com>
2025-01-27 23:08:58 +0100

    crampté

--- Assets/Scripts/CameraMouvement.cs
+++ Assets/Scripts/CameraMouvement.cs
@@ -8,21 +8,25 @@ public class CameraMouvement : MonoBehaviour
     private Vector2 _moveInput;
     public void HandleCameraMovement(InputAction.CallbackContext context)
     {
+        print("Handle Camera");
         _moveInput = Vector2.zero;
         if (context.phase == InputActionPhase.Performed)
         {
             _moveInput = context.ReadValue<Vector2>();
+            print("Move Input");
+            print(_moveInput);
         }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        print("START CAMERA MY BOYYY");
     }
 
     // Update is called once per frame
     void Update()
     {
+        print("Update");
         var mouvement = moveSpeed * Time.deltaTime * (new Vector3(_moveInput.y, 0, -_moveInput.x));
         transform.Translate(mouvement, Space.World);
     }
